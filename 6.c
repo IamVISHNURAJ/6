548fc18dd38c0a486a273f46da70d9c800475b17
IamVISHNURAJ <50854010+IamVISHNURAJ@users.noreply.github.com>
2019-05-23 13:56:44 +0530

    Add files via upload

---
+++ 6.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int y;
+    scanf("%d",&y);
+    if(year%4 == 0)
+    {
+        if( y%100 == 0)
+        {
+            if ( y%400 == 0)
+                printf("yes");
+            else
+                printf("no");
+        }
+        else
+            printf("yes");
+    }
+    else
+        printf("no");
+    
+    return 0;
+}
